66eae964c588e352e343ccfc1396a02cead45170
Jefferson Moreira Da Silva <datsinnech@gmail.com>
2022-03-03 23:05:43 +0100

    Removed contact and sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'temp/home.html')
-
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'home.html')
 
 # Create your views here.
